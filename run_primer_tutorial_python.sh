f03bce55d6cf7ca512039355d1b69d8e2ea87f31
Kuba Kucharski <kuba.kucharski@gmail.com>
2020-10-06 12:58:01 +0200

    Update run_primer_tutorial_python.sh

--- run_primer_tutorial_python.sh
+++ run_primer_tutorial_python.sh
@@ -1,48 +1,6 @@
 #!/bin/bash
 
-# PREREQS
-# You're going to need jq installed
-# brew install jq or apt-get install jq
-
-_ostype="$(uname -s)"
-
-
-
-# CLEANUPS
-
-# Remove last install of yagna
-
-
-if [ "$_ostype" = "Darwin" ]; then
-	rm -rf $HOME/Library/Application\ Support/GolemFactory.yagna
-fi
-
-if [ "$_ostype" = "Linux" ]; then
-	rm -rf $HOME/.local/share/yagna 
-fi
-
-# Remove last install of blender and env
-rm -rf test_run || true
-killall yagna
-# Remove Python env
-
-rm -rf ~/.envs/yagna-python-tutorial
-
-mkdir test_run
-cd test_run
-python3 --version
-curl -sSf https://join.golem.network/as-requestor | bash -
-yagna --version
-yagna service run > yagna.logs 2>&1 &
-echo "Waiting 10 seconds for yagna to initialise"
-sleep 10
-
-# TUTORIAL STEPS
-
-yagna app-key create requestor
-yagna app-key list
-yagna payment init -r
-yagna payment status
+cd ~/test_run
 python3 -m venv ~/.envs/yagna-python-tutorial
 source ~/.envs/yagna-python-tutorial/bin/activate
 pip3 install -U pip
